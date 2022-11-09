802d347ed67e93417b23f4f107a03a7e065bc8fa
Leonardo Guedes Ferreira <leoguedes99@gmail.com>
2022-11-09 20:01:00 -0300

    adicionandoa pagina games, enviando questoes para o componente que ira renderizar

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,13 +1,44 @@
 import React from 'react';
+import propTypes from 'prop-types';
+import { getQuestions } from '../services/api';
+import Questions from '../components/Questions';
 
-class Game extends React.Component {
+export default class Game extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      questions: [],
+    };
+  }
+
+  componentDidMount() {
+    this.gettingQuestions();
+  }
+
+  gettingQuestions = async () => {
+    const FAILED_CODE = 3;
+    const returnedQuestions = await getQuestions();
+    const { history } = this.props;
+    if (returnedQuestions.response_code === FAILED_CODE) {
+      localStorage.removeItem('token');
+      history.push('/');
+    } else {
+      this.setState({
+        questions: returnedQuestions.results,
+      });
+    }
+  };
+
   render() {
+    const { questions } = this.state;
     return (
       <div>
-        <h2>Trivia</h2>
+        {questions.length > 0 && <Questions
+          questions={ questions }
+        />}
       </div>
     );
   }
 }
-
-export default Game;
+Game.propTypes = {
+  history: propTypes.objectOf(propTypes.any) }.isRequired;
