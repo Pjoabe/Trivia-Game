b215b1fd39ab822c8fe78bfd7f202f733c60d882
Mateus M. S. A. Figueiredo <mateusmelosf@gmail.com>
2022-11-11 15:22:09 -0300

    requisito 17

--- src/tests/Feedback.test.js
+++ src/tests/Feedback.test.js
@@ -5,22 +5,37 @@ import App from "../App";
 import renderWithRouterAndRedux from "./helpers/renderWithRouterAndRedux"
 
 describe('<feedback>',()=> {
-it('',()=> {
+
+  
+it('aparece messagem Well Done! se score for maior que 3',()=> {
+  const {history} = renderWithRouterAndRedux(<App/>, {player: {score: 4, assertions: 80, gravatarEmail: 'matues@gmail.com', name: 'batman'}}, '/feedback')
+  const {pathname} = history.location
+  expect(pathname).toBe('/feedback')
+  const message = screen.getByText('Well Done!')
+  expect(message).toBeInTheDocument()
 
 });
-it('',() => {
-
+it('aparece messagem Could be better... se score for maior que ',() => {
+  renderWithRouterAndRedux(<App/>, {player: {score: 2, assertions: 80, gravatarEmail: 'matues@gmail.com', name: 'batman'}}, '/feedback')
+  const message = screen.getByText('Could be better...')
+  expect(message).toBeInTheDocument()
 });
-it('',() => {
-
+it('se o botao play again volta para pagina inicial',() => {
+  const {history} = renderWithRouterAndRedux(<App/>, {player: {score: 2, assertions: 80, gravatarEmail: 'matues@gmail.com', name: 'batman'}}, '/feedback')
+  const btn = screen.getByRole('button', {name: 'Play Again'})
+  expect(btn).toBeInTheDocument()
+  userEvent.click(btn)
+  const {pathname} = history.location
+  expect(pathname).toBe('/')
+  
 });
-it('',() => {
-
+it('se o botao ranking vai para pagina de ranking',() => {
+  const {history} = renderWithRouterAndRedux(<App/>, {player: {score: 2, assertions: 80, gravatarEmail: 'matues@gmail.com', name: 'batman'}}, '/feedback')
+  const btn = screen.getByRole('button', {name: 'Ranking'})
+  expect(btn).toBeInTheDocument()
+  userEvent.click(btn)
+  const {pathname} = history.location
+  expect(pathname).toBe('/ranking')
 });
-it('',() => {
 
-});
-it('',() => {
-
-});
 })
