40a5fd8612243c922ddeb47b462a352268d5b12d
Igor Carvalho <igorr.ackerman@gmail.com>
2022-11-11 18:51:52 -0300

    Patch pra consertar quebra do app

--- src/tests/Feedback.test.js
+++ src/tests/Feedback.test.js
@@ -2,7 +2,9 @@ import { screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Feedback from "../pages/Feedback";
 import App from "../App";
-import renderWithRouterAndRedux from "./helpers/renderWithRouterAndRedux"
+import renderWithRouterAndRedux from "./helpers/renderWithRouterAndRedux";
+import playedStore from './helpers/mockStore';
+import { act } from "react-dom/test-utils";
 
 describe('<feedback>',()=> {
 
@@ -36,6 +38,24 @@ it('se o botao ranking vai para pagina de ranking',() => {
   userEvent.click(btn)
   const {pathname} = history.location
   expect(pathname).toBe('/ranking')
+});
+it('O botão do ranking funciona mesmo com mais de um jogador', () => {
+  const { history } = renderWithRouterAndRedux(
+    <App />, { player: playedStore }, '/feedback'
+  );
+
+  const rankBtn = screen.getByRole('button', { name: /ranking/i })
+  userEvent.click(rankBtn);
+
+  const play = screen.getByRole('button', { name: /back to main screen/i })
+  userEvent.click(play);
+
+  act(() => {
+    history.push('/feedback');
+  })
+  const rankkBtn = screen.getByRole('button', { name: /ranking/i })
+  userEvent.click(rankkBtn);
+  expect(history.location.pathname).toBe('/ranking')
 });
 
 })
